fbcf44649802db6d9cd59a306b8835a342873a04
andrijanikic23 <andrijanikic001@gmail.com>
2025-12-29 19:47:58 +0100

    * Fixed table and admin display

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,13 +3,21 @@
 
 <style>
 
+    .fixed {
+        table-layout:fixed;
+    }
+
+    .fixed td {
+        overflow:hidden;
+    }
+
 </style>
 
 @section('content')
 
     <div>
 
-        <table class="container mt-4 table table-dark table-hover">
+        <table class="container mt-4 table table-dark table-hover fixed">
             <tr>
                 <th>id</th>
                 <th>Ime i prezime</th>
@@ -24,11 +32,15 @@
                     <th>{{ $user->name }}</th>
                     <th>{{ $user->email }}</th>
                     <th>{{ $user->role }}</th>
-                    <form action="{{ route('admin.role', ['user' => $user->id]) }}" method="POST">
-                        @csrf
-                        @method('PATCH')
-                        <th class="text-center"><button type="submit">Promeni ulogu u {{ \App\Http\AdminHelper::displayButtonRole($user->role) }}</button></th>
-                    </form>
+                    @if($user->role == 'staff' || $user->role == 'patient')
+                        <form action="{{ route('admin.role', ['user' => $user->id]) }}" method="POST">
+                            @csrf
+                            @method('PATCH')
+                            <th class="text-center"><button class="btn btn-light" style="width:200px; height:40px;" type="submit">Promeni ulogu u {{ \App\Http\AdminHelper::displayButtonRole($user->role) }}</button></th>
+                        </form>
+                    @else
+                        <th class="text-center">ADMIN</th>
+                    @endif
 
                 </tr>
             @endforeach
